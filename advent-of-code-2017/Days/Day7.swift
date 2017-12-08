92c2028a733fecd33410c9942b571c9e8b52e804
Søren Sønderby Nielsen <ssni@itu.dk>
2017-12-08 23:48:21 +0100

    Day 7 Part 1

---
+++ advent-of-code-2017/Days/Day7.swift
@@ -0,0 +1,42 @@
+//
+//  Day7.swift
+//  advent-of-code-2017
+//
+//  Created by Soren Sonderby Nielsen on 07/12/2017.
+//  Copyright © 2017 sonderby Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Day7: Day {
+    static func part1(input: String) -> String {
+        let programs = parse(input)
+
+        let programNames = Set(programs.keys)
+        let mentionedNeighbors = Set(programs.values.map { $0.0 }.flatMap { $0 })
+        let top = programNames.symmetricDifference(mentionedNeighbors)
+
+        return top.first!
+    }
+
+    static func part2(input: String) -> String {
+        return ""
+    }
+
+    private static func parse(_ input: String) -> [String: ([String], Int)] {
+        let lines = input.components(separatedBy: .newlines).filter { !$0.isEmpty }
+
+        var programs = [String: ([String], Int)]()
+
+        lines.forEach {
+            let parts = $0.components(separatedBy: " -> ")
+            let program = parts.first!.components(separatedBy: .whitespaces)
+            let name = program.first!
+            let weight = Int(program.last!.replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: ""))!
+            let programNeighbors = parts.count == 2 ? (parts.last?.components(separatedBy: ", ") ?? []) : []
+            programs[name] = (programNeighbors, weight)
+        }
+
+        return programs
+    }
+}
--- advent-of-code-2017/main.swift
+++ advent-of-code-2017/main.swift
@@ -29,7 +29,7 @@ struct Helper {
     }
 }
 
-let input = Helper.readDay(6)
-let day = Day6.self
+let input = Helper.readDay(7)
+let day = Day7.self
 print(day.part1(input: input))
 print(day.part2(input: input))
