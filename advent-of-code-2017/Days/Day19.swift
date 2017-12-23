c7de5dbb4c9289e2bbdedafe98adf7cac6a5d212
Søren Sønderby Nielsen <ssn@e-conomic.com>
2017-12-23 18:13:27 +0100

    Day 19 Part 1

--- advent-of-code-2017/Days/Day19.swift
+++ advent-of-code-2017/Days/Day19.swift
@@ -8,29 +8,52 @@
 
 import Foundation
 
+fileprivate extension Dictionary where Key == Int, Value == [Int: Character] {
+    subscript(point point: Day19.Point) -> Character? {
+        get {
+            return self[point.x]?[point.y]
+        }
+        set {
+            if newValue == nil {
+                self[point.x]?.removeValue(forKey: point.y)
+            } else {
+                self[point.x, default: [:]][point.y] = newValue!
+            }
+        }
+    }
+}
+
+func ==(lhs: Day19.Point, rhs: Day19.Point) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
 struct Day19: Day {
     typealias Point = (x: Int, y: Int)
     typealias Grid = [Int: [Int: Character]]
 
     static func part1(input: String) -> String {
         let grid = parse(input)
-        let startingPoint: Point = (x: 0, y: grid[0]!.first(where: { $0.value == "|" })!.key)
+        let start = startingPoint(in: grid)
 
-        return route(in: grid, from: startingPoint, visitedPackages: "")
+        return route(in: grid, from: start, visitedPackages: "")
     }
 
     static func part2(input: String) -> String {
         return ""
     }
 
+    private static func startingPoint(in grid: Grid) -> Point {
+        return (x: 0, y: grid[0]!.first(where: { $0.value == "|" })!.key)
+    }
+
     private static func route(in grid: Grid, from point: Point, comingFrom pointBefore: Point = (x: -1, y: -1), visitedPackages: String) -> String {
         guard let next = step(from: point, comingFrom: pointBefore, in: grid) else {
             return visitedPackages
         }
-        print(point)
-        let letter = String(grid[next.x]![next.y]!)
 
-        if ("A"..."Z").contains(letter) {
+        let letter = String(grid[point: next]!)
+
+        if !["+", "|", "-"].contains(letter) {
             return route(in: grid, from: next, comingFrom: point, visitedPackages: visitedPackages + letter)
         } else {
             return route(in: grid, from: next, comingFrom: point, visitedPackages: visitedPackages)
@@ -38,12 +61,41 @@ struct Day19: Day {
     }
 
     private static func step(from point: Point, comingFrom pointBefore: Point, in grid: Grid) -> Point? {
+        let letterBefore = grid[point: pointBefore] ?? "1"
+        let letterCurrent = grid[point: point]!
         let neighbors = generateNeighbors(for: point)
-        let nonVisitedNeighbors = neighbors.filter { ($0.x >= 0 && $0.y >= 0) && ($0.x != pointBefore.x || $0.y != pointBefore.y) }
+        let nonVisitedNeighbors = neighbors.filter {
+            guard let letterNeighbor = grid[point: $0] else {
+                return false
+            }
 
-        return nonVisitedNeighbors.first {
-            grid[$0.x]?[$0.y] != nil
+            if $0 == pointBefore {
+                return false
+            }
+
+            let sameAxis = $0.x == pointBefore.x || $0.y == pointBefore.y
+
+            switch (letterBefore, letterCurrent, letterNeighbor) {
+            case (_, _, let letter) where ("A"..."Z").contains(String(letter)) && sameAxis:
+                return true
+            case (_, let letter, _) where ("A"..."Z").contains(String(letter)) && sameAxis:
+                return true
+            case (let letter, _, _) where ("A"..."Z").contains(String(letter)) && sameAxis:
+                return true
+            case (_, "+", _):
+                return true
+            case (_, _, "+"):
+                return true
+            case (_, _, "-") where sameAxis:
+                return true
+            case (_, _, "|") where sameAxis:
+                return true
+            default:
+                return false
+            }
         }
+
+        return nonVisitedNeighbors.first
     }
 
     private static func generateNeighbors(for point: Point) -> [Point] {
