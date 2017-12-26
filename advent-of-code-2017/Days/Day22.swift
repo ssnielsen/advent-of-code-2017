fa94a0fe5634810ad5c5f0178a382ddda11ef05d
Søren Sønderby Nielsen <ssn@e-conomic.com>
2017-12-26 12:53:21 +0100

    Day 22 Part 1+2

--- advent-of-code-2017/Days/Day22.swift
+++ advent-of-code-2017/Days/Day22.swift
@@ -9,20 +9,12 @@
 import Foundation
 
 struct Day22: Day {
-    static let testInput = """
-..#
-#..
-...
-"""
-
     static func part1(input: String) -> String {
-        let input = testInput
         let infected = parse(input)
         let carrier = findStartPoint(in: input)
-        let direction = Direction.up
+        let direction = Direction.north
 
-        let result = (0..<70).reduce((infected: infected, carrier: carrier, direction: direction, infectionBursts: 0)) { state, _ in
-            print(state)
+        let result = (0..<10_000).reduce((infected: infected, carrier: carrier, direction: direction, infectionBursts: 0)) { state, _ in
             let next = burst(carrierPosition: state.carrier, infected: state.infected, direction: state.direction)
             return (next.infected, next.carrier, next.direction, state.infectionBursts + (next.infectedInBurst ? 1 : 0))
         }
@@ -31,7 +23,17 @@ struct Day22: Day {
     }
 
     static func part2(input: String) -> String {
-        return ""
+        let infected = parse(input).map { ($0, NodeState.infected) }
+        let infectedAdvanced = Dictionary(tuples: infected)
+        let carrier = findStartPoint(in: input)
+        let direction = Direction.north
+
+        let result = (0..<10_000_000).reduce((infected: infectedAdvanced, carrier: carrier, direction: direction, infectionBursts: 0)) { state, _ in
+            let next = burst2(carrierPosition: state.carrier, infected: state.infected, direction: state.direction)
+            return (next.infected, next.carrier, next.direction, state.infectionBursts + (next.infectedInBurst ? 1 : 0))
+        }
+
+        return "\(result.infectionBursts)"
     }
 
     private static func burst(carrierPosition: Position, infected: Set<Position>, direction: Direction) -> (carrier: Position, infected: Set<Position>, direction: Direction, infectedInBurst: Bool) {
@@ -40,9 +42,34 @@ struct Day22: Day {
         let newInfected = isCarrierPositionInfected ? infected.subtracting([carrierPosition]) : infected.union([carrierPosition])
         let moveVector = newDirection.moveVector
         let newCarrierPosition = Position(x: carrierPosition.x + moveVector.x, y: carrierPosition.y + moveVector.y)
-//        print((newCarrierPosition, newDirection))
 
         return (newCarrierPosition, newInfected, newDirection, !isCarrierPositionInfected)
+    }
+
+    private static func burst2(carrierPosition: Position, infected: [Position: NodeState], direction: Direction) -> (carrier: Position, infected: [Position: NodeState], direction: Direction, infectedInBurst: Bool) {
+
+        let carrierPositionState = infected[carrierPosition] ?? .clean
+        var turnings = [Turn]()
+        switch carrierPositionState {
+        case .clean:
+            turnings.append(.left)
+        case .weakened:
+            break
+        case .infected:
+            turnings.append(.right)
+        case .flagged:
+            // Rotate 180 degrees
+            turnings.append(.left)
+            turnings.append(.left)
+        }
+
+        let newDirection = turnings.reduce(direction) { $0.turn($1) }
+        var newInfected = infected
+        newInfected[carrierPosition] = carrierPositionState.nextState()
+        let moveVector = newDirection.moveVector
+        let newCarrierPosition = Position(x: carrierPosition.x + moveVector.x, y: carrierPosition.y + moveVector.y)
+
+        return (newCarrierPosition, newInfected, newDirection, carrierPositionState.nextState() == .infected)
     }
 
     private static func findStartPoint(in input: String) -> Position {
@@ -87,50 +114,66 @@ struct Day22: Day {
     }
 
     enum Direction: CustomStringConvertible {
-        case up, right, down, left
+        case north, east, south, west
 
         func turn(_ turn: Turn) -> Direction {
             switch (self, turn) {
-            case (.up, .left):
-                return .left
-            case (.up, .right):
-                return .right
-            case (.right, .left):
-                return .up
-            case (.right, .right):
-                return .down
-            case (.down, .left):
-                return .right
-            case (.down, .right):
-                return .left
-            case (.left, .left):
-                return .down
-            case (.left, .right):
-                return .up
+            case (.north, .left):
+                return .west
+            case (.north, .right):
+                return .east
+            case (.east, .left):
+                return .north
+            case (.east, .right):
+                return .south
+            case (.south, .left):
+                return .east
+            case (.south, .right):
+                return .west
+            case (.west, .left):
+                return .south
+            case (.west, .right):
+                return .north
             }
         }
 
         var moveVector: Position {
             switch self {
-            case .up:
-                return Position(x: 0, y: 1)
-            case .right:
-                return Position(x: 1, y: 0)
-            case .down:
+            case .north:
                 return Position(x: 0, y: -1)
-            case .left:
+            case .east:
+                return Position(x: 1, y: 0)
+            case .south:
+                return Position(x: 0, y: 1)
+            case .west:
                 return Position(x: -1, y: 0)
             }
         }
 
         var description: String {
             switch self {
-            case .up: return "Up"
-            case .right: return "Right"
-            case .down: return "Down"
-            case .left: return "Left"
+            case .north: return "Up"
+            case .east: return "Right"
+            case .south: return "Down"
+            case .west: return "Left"
             }
         }
     }
 
+    enum NodeState {
+        case weakened, infected, flagged, clean
+
+        func nextState() -> NodeState {
+            switch self {
+            case .weakened:
+                return .infected
+            case .infected:
+                return .flagged
+            case .flagged:
+                return .clean
+            case .clean:
+                return .weakened
+            }
+        }
+    }
 }
